5bbae0d914ec2958ec4e02e8de3f73a7cf6e83f3
Peter Arato <it.arato@gmail.com>
2025-05-21 07:49:26 -0400

    Terminal mode

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,10 @@ module Main where
 
 import Control.Exception qualified as Exception
 import System.Console.Terminal.Size qualified as TSize
+import System.IO (BufferMode (..), hSetBuffering, hSetEcho, stdin)
 import System.IO.Error qualified as IOError
+import System.Posix.IO (stdInput)
+import System.Posix.Terminal
 
 clearScreen :: IO ()
 clearScreen = putStr "\ESC[2J"
@@ -21,17 +24,46 @@ maybeToIOException :: String -> Maybe a -> IO a
 maybeToIOException _ (Just a) = return a
 maybeToIOException msg _ = Exception.throwIO . IOError.userError $ msg
 
+enableTerminalRawMode :: IO TerminalAttributes
+enableTerminalRawMode = do
+  oldConfig <- getTerminalAttributes stdInput
+
+  hSetBuffering stdin NoBuffering
+  hSetEcho stdin False
+
+  let newConfig =
+        oldConfig
+          `withoutMode` EnableEcho
+          `withoutMode` ProcessInput
+          `withoutMode` KeyboardInterrupts
+          `withoutMode` InterruptOnBreak
+
+  setTerminalAttributes stdInput newConfig Immediately
+
+  return oldConfig
+
+restoreTerminalMode :: TerminalAttributes -> IO ()
+restoreTerminalMode oldConfig = do
+  setTerminalAttributes stdInput oldConfig Immediately
+  hSetBuffering stdin LineBuffering
+  hSetEcho stdin True
+
 main :: IO ()
 main = catchIOException $ do
-  clearScreen
-  _termSize <- termSize >>= maybeToIOException "Failed reading terminal size"
-  cursorToXY 10 20
-  putStr $ "Hello, Haskell!" <> show _termSize
-  cursorToXY 10 20
-  putStr "XXX"
-  cursorToXY 11 20
-  putStr "XXX"
-  return ()
+  oldTerminalConfig <- enableTerminalRawMode
+  Exception.finally
+    ( do
+        clearScreen
+        _termSize <- termSize >>= maybeToIOException "Failed reading terminal size"
+        cursorToXY 10 20
+        putStr $ "Hello, Haskell!" <> show _termSize
+        cursorToXY 10 20
+        putStr "XXX"
+        cursorToXY 11 20
+        putStr "XXX"
+        return ()
+    )
+    (restoreTerminalMode oldTerminalConfig)
   where
     catchIOException :: IO () -> IO ()
     catchIOException ioAction = Exception.catch ioAction $ \e -> putStrLn "Error: " >> print @IOError e
