732c9236acfe8780e7502f0a970e11e4ae9faa79
Peter Arato <it.arato@gmail.com>
2025-05-21 08:38:19 -0400

    Game loop

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,20 @@ module Main where
 
 import Control.Concurrent
 import Control.Exception qualified as Exception
+import Control.Monad
 import System.Console.Terminal.Size qualified as TSize
 import System.IO (BufferMode (..), hFlush, hSetBuffering, hSetEcho, stdin, stdout)
 import System.IO.Error qualified as IOError
 import System.Posix.IO (stdInput)
 import System.Posix.Terminal
+
+data GameState = MakeGameState
+  { frame :: (Int, Int),
+    direction :: Int,
+    parts :: [(Int, Int)],
+    dead :: Bool
+  }
+  deriving (Show)
 
 clearScreen :: IO ()
 clearScreen = putStr "\ESC[2J"
@@ -14,8 +23,8 @@ clearScreen = putStr "\ESC[2J"
 cursorToXY :: Int -> Int -> IO ()
 cursorToXY row col = putStr $ "\ESC[" ++ show row ++ ";" ++ show col ++ "H"
 
-termSize :: IO (Maybe (Int, Int))
-termSize = do
+getTermSize :: IO (Maybe (Int, Int))
+getTermSize = do
   window <- TSize.size
   return $ case window of
     Just TSize.Window {TSize.height = h, TSize.width = w} -> Just (w, h)
@@ -49,21 +58,45 @@ restoreTerminalMode oldConfig = do
   hSetBuffering stdin LineBuffering
   hSetEcho stdin True
 
+gameLoop :: GameState -> IO ()
+gameLoop state = do
+  if dead state
+    then do
+      putStr "Second iteration"
+      hFlush stdout
+      return ()
+    else do
+      -- cursorToXY 10 20
+      putStr "First iteration"
+      hFlush stdout
+      threadDelay 1_000_000
+      -- cursorToXY 10 20
+      -- putStr "XXX"
+      -- cursorToXY 11 20
+      -- putStr "XXX"
+      -- mapM (\n -> putStr (show n) >> hFlush stdout >> guard (n <= 5) >> threadDelay 300_000) [1 .. 10]
+      gameLoop state {direction = direction state + 1, dead = True}
+
+-- cursorToXY 10 20
+-- putStr "Hello, Haskell!"
+-- hFlush stdout
+-- threadDelay 3_000_000
+-- cursorToXY 10 20
+-- putStr "XXX"
+-- cursorToXY 11 20
+-- putStr "XXX"
+-- mapM (\n -> putStr (show n) >> hFlush stdout >> guard (n <= 5) >> threadDelay 300_000) [1 .. 10]
+-- return ()
+
 main :: IO ()
 main = catchIOException $ do
   oldTerminalConfig <- enableTerminalRawMode
   Exception.finally
     ( do
         clearScreen
-        _termSize <- termSize >>= maybeToIOException "Failed reading terminal size"
-        cursorToXY 10 20
-        putStr $ "Hello, Haskell!" <> show _termSize
-        hFlush stdout
-        threadDelay 3_000_000
-        cursorToXY 10 20
-        putStr "XXX"
-        cursorToXY 11 20
-        putStr "XXX"
+        termSize <- getTermSize >>= maybeToIOException "Failed reading terminal size"
+        let gameState = MakeGameState termSize 0 [] False
+        gameLoop gameState
         return ()
     )
     (restoreTerminalMode oldTerminalConfig)
