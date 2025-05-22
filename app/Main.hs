a0d6f594d319c475e84f13ba090189e7f3d8d8c1
Peter Arato <it.arato@gmail.com>
2025-05-22 08:57:01 -0400

    Eat

--- app/Main.hs
+++ app/Main.hs
@@ -27,6 +27,12 @@ pattern WEST = 3
 pattern PART :: Char
 pattern PART = '█'
 
+pattern FOOD :: Char
+pattern FOOD = '$'
+
+pattern GROW_SIZE :: Int
+pattern GROW_SIZE = 3
+
 newtype Coord = Coord (Int, Int) deriving (Show, Eq)
 
 instance Semigroup Coord where
@@ -42,13 +48,16 @@ data GameState = MakeGameState
     dead :: Bool,
     grow :: Int,
     rng :: StdGen,
-    food :: [Coord]
+    food :: Coord
   }
   deriving (Show)
 
 -- TODO replace RNG seed with time
 initGameState :: Coord -> GameState
-initGameState frame@(Coord (w, h)) = MakeGameState frame NORTH [Coord (w `div` 2, h `div` 2)] False 3 (mkStdGen 1337) []
+initGameState frame@(Coord (w, h)) = MakeGameState frame NORTH [Coord (w `div` 2, h `div` 2)] False 3 rng' foodCoord
+  where
+    rng = mkStdGen 1337
+    (foodCoord, rng') = randCoord rng frame
 
 clearScreen :: IO ()
 clearScreen = putStr "\ESC[2J"
@@ -112,6 +121,7 @@ directionMap _ = error "Invalid direction"
 newHead :: GameState -> Coord
 newHead state = directionMap (direction state) <> head (parts state)
 
+-- TODO Have a stdin flush
 nonBlockGetChar :: IO (Maybe Char)
 nonBlockGetChar = do
   stdin_ready <- hReady stdin
@@ -141,6 +151,7 @@ truncateTail [] True = []
 truncateTail [_] True = []
 truncateTail (x : xs) True = x : truncateTail xs True
 
+-- TODO Add a wrapper that calls this until it does not match a provided list of coords (no new coord on snake).
 randCoord :: (RandomGen r) => r -> Coord -> (Coord, r)
 randCoord rng (Coord (w, h)) = (coord, rng'')
   where
@@ -159,23 +170,32 @@ gameLoop state = do
       let needShrink = grow state == 0
       let newParts = newHead' : truncateTail (parts state) needShrink
       let newGrow = if needShrink then grow state else grow state - 1
+      let didEatFood = newHead' == food state
+      let (newFood, newRng) = if didEatFood then randCoord (rng state) (frame state) else (food state, rng state)
+      let newGrow' = if didEatFood then newGrow + GROW_SIZE else newGrow
       cursorToXY newHead'
       putChar PART
       when needShrink $
         do
           cursorToXY $ last (parts state)
           putChar ' '
+      when didEatFood $
+        do
+          cursorToXY newFood
+          putChar FOOD
       hFlush stdout
       input <- nonBlockGetChar
       let newDirection' = newDirection input (direction state)
       -- TODO Die also when reaching frame.
       let newDead = dead state || didHitExit input || didBiteItself newHead' (parts state)
       threadDelay 100_000
-      gameLoop state {parts = newParts, direction = newDirection', dead = newDead, grow = newGrow}
+      gameLoop state {parts = newParts, direction = newDirection', dead = newDead, grow = newGrow', food = newFood, rng = newRng}
 
 drawBaseState :: GameState -> IO ()
 drawBaseState state = do
   mapM_ drawPart (parts state)
+  cursorToXY $ food state
+  putChar FOOD
   hFlush stdout
   return ()
   where
