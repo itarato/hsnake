d177c263865ae1fea0b422e66648a10a3273dd56
Peter Arato <it.arato@gmail.com>
2025-05-21 21:21:05 -0400

    Moving.

--- app/Main.hs
+++ app/Main.hs
@@ -1,33 +1,62 @@
+{-# LANGUAGE PatternSynonyms #-}
+
 module Main where
 
 import Control.Concurrent
 import Control.Exception qualified as Exception
 import Control.Monad
+import GHC.Conc (par)
 import System.Console.Terminal.Size qualified as TSize
 import System.IO (BufferMode (..), hFlush, hSetBuffering, hSetEcho, stdin, stdout)
 import System.IO.Error qualified as IOError
 import System.Posix.IO (stdInput)
 import System.Posix.Terminal
 
+newtype Coord = Coord (Int, Int) deriving (Show)
+
+instance Semigroup Coord where
+  (<>) (Coord (x1, y1)) (Coord (x2, y2)) = Coord (x1 + x2, y1 + y2)
+
+instance Monoid Coord where
+  mempty = Coord (0, 0)
+
+pattern NORTH :: Int
+pattern NORTH = 0
+
+pattern EAST :: Int
+pattern EAST = 1
+
+pattern SOUTH :: Int
+pattern SOUTH = 2
+
+pattern WEST :: Int
+pattern WEST = 3
+
+pattern PART :: String
+pattern PART = "X"
+
 data GameState = MakeGameState
-  { frame :: (Int, Int),
+  { frame :: Coord,
     direction :: Int,
-    parts :: [(Int, Int)],
+    parts :: [Coord],
     dead :: Bool
   }
   deriving (Show)
 
+initGameState :: Coord -> GameState
+initGameState frame@(Coord (w, h)) = MakeGameState frame NORTH [Coord (w `div` 2, h `div` 2)] False
+
 clearScreen :: IO ()
 clearScreen = putStr "\ESC[2J"
 
-cursorToXY :: Int -> Int -> IO ()
-cursorToXY row col = putStr $ "\ESC[" ++ show row ++ ";" ++ show col ++ "H"
+cursorToXY :: Coord -> IO ()
+cursorToXY (Coord (col, row)) = putStr $ "\ESC[" ++ show row ++ ";" ++ show col ++ "H"
 
-getTermSize :: IO (Maybe (Int, Int))
+getTermSize :: IO (Maybe Coord)
 getTermSize = do
   window <- TSize.size
   return $ case window of
-    Just TSize.Window {TSize.height = h, TSize.width = w} -> Just (w, h)
+    Just TSize.Window {TSize.height = h, TSize.width = w} -> Just (Coord (w, h))
     Nothing -> Nothing
 
 maybeToIOException :: String -> Maybe a -> IO a
@@ -58,24 +87,44 @@ restoreTerminalMode oldConfig = do
   hSetBuffering stdin LineBuffering
   hSetEcho stdin True
 
+putStrAndFlush :: String -> IO ()
+putStrAndFlush s = do
+  putStr s
+  hFlush stdout
+
+directionMap :: Int -> Coord
+directionMap NORTH = Coord (0, -1)
+directionMap EAST = Coord (1, 0)
+directionMap SOUTH = Coord (0, 1)
+directionMap WEST = Coord (-1, 0)
+directionMap _ = error "Invalid direction"
+
+newHead :: GameState -> Coord
+newHead state = directionMap (direction state) <> head (parts state)
+
 gameLoop :: GameState -> IO ()
 gameLoop state = do
-  if dead state
+  if length (parts state) > 10
     then do
-      putStr "Second iteration"
-      hFlush stdout
-      return ()
+      putStrAndFlush "Game Over"
     else do
-      -- cursorToXY 10 20
-      putStr "First iteration"
+      let newHead' = newHead state
+      let newParts = newHead' : parts state
+      cursorToXY newHead'
+      putStr PART
       hFlush stdout
       threadDelay 1_000_000
-      -- cursorToXY 10 20
-      -- putStr "XXX"
-      -- cursorToXY 11 20
-      -- putStr "XXX"
-      -- mapM (\n -> putStr (show n) >> hFlush stdout >> guard (n <= 5) >> threadDelay 300_000) [1 .. 10]
-      gameLoop state {direction = direction state + 1, dead = True}
+      gameLoop state {parts = newParts, dead = True}
+
+drawBaseState :: GameState -> IO ()
+drawBaseState state = do
+  mapM_ drawPart (parts state)
+  hFlush stdout
+  return ()
+  where
+    drawPart coord = do
+      cursorToXY coord
+      putStr PART
 
 -- cursorToXY 10 20
 -- putStr "Hello, Haskell!"
@@ -95,7 +144,8 @@ main = catchIOException $ do
     ( do
         clearScreen
         termSize <- getTermSize >>= maybeToIOException "Failed reading terminal size"
-        let gameState = MakeGameState termSize 0 [] False
+        let gameState = initGameState termSize
+        drawBaseState gameState
         gameLoop gameState
         return ()
     )
